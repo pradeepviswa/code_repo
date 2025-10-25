b43f093b3461ebb63e138353bb8416c03d283e4b
Pradeep Viswanathan <pradeep.viswa@gmail.com>
2025-10-25 17:40:04 +0530

    nlb

---
+++ Template/Network_Load_Balancer/Test TCP.ps1
@@ -0,0 +1,24 @@
+﻿
+#TEST TCP
+$server = $NLBDNS
+$port = 6381
+
+$tcp = New-Object System.Net.Sockets.TcpClient
+$tcp.Connect($server, $port)
+
+$stream = $tcp.GetStream()
+$reader = New-Object System.IO.StreamReader($stream)
+$writer = New-Object System.IO.StreamWriter($stream)
+
+# Send a message
+$writer.WriteLine("Hello TCP Server")
+$writer.Flush()
+
+# Read response
+$response = $reader.ReadLine()
+Write-Output "Server replied: $response"
+
+# Close connection
+$reader.Close()
+$writer.Close()
+$tcp.Close()
---
+++ Template/Network_Load_Balancer/Test UDP.ps1
@@ -0,0 +1,14 @@
+﻿#UDP
+$server = $NLBDNS
+$port = 6380
+
+$udpClient = New-Object System.Net.Sockets.UdpClient
+$udpClient.Connect($server, $port)
+
+# Send a test message
+$message = [System.Text.Encoding]::ASCII.GetBytes("Hello UDP Server")
+$udpClient.Send($message, $message.Length)
+
+Write-Output "Message sent to $($server):$($port) via UDP"
+
+$udpClient.Close()
